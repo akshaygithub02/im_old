394b2c0a88f9061881ff64d885c0da00314b6127
Akshay <akshay.github02@gmail.com>
2018-06-23 07:18:47 +0530

    sql conn change

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php 
+	$conn = mysqli_connect('localhost','root','','tekticks_inventoryMgmt'); //
 	// Database connection file.
-	$conn = mysqli_connect('tekticks.com','tekticks_invmgmt','demo@123','tekticks_inventoryMgmt'); // Server/Cpanel db connection.
+	//$conn = mysqli_connect('tekticks.com','tekticks_invmgmt','demo@123','tekticks_inventoryMgmt'); 
+	// Server/Cpanel db connection.
 	//$conn = mysqli_connect('localhost','root','','intnet'); // Localhost db connection
 ?>
